cba6eb482ddebbb486b70c23e55fc427767bcb6a
stipe-jurkovic <sjurko00@fesb.hr>
2024-01-22 11:40:55 +0100

    necuuuuu

--- app/plot_flats.py
+++ app/plot_flats.py
@@ -25,10 +25,11 @@ for index, row in df.iterrows():
 croatia_map = folium.Map(location=[45.1, 15.5], zoom_start=7)
 
 # Add markers for each flat
+marker_cluster = MarkerCluster(max_cluster_radius=40000).add_to(croatia_map)  # Increase the max_cluster_radius value
 
 # Add markers for each flat to the cluster, not the map
 for row in data:
-    folium.Marker([row['lat'], row['lng']], popup=row['url']).add_to(croatia_map)
+    folium.Marker([row['lat'], row['lng']], popup=row['url']).add_to(marker_cluster)
 
 # Save the map to an HTML file
 croatia_map.save('flats_map.html')
